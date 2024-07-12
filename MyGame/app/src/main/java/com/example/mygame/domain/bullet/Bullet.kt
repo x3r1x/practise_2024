112c972a3494817dbabee5482ca18734ae7322b7
impronez <orzaev7312@gmail.com>
2024-07-12 15:50:23 +0300

    Поменял пулю на круглую

--- MyGame/app/src/main/java/com/example/mygame/domain/bullet/Bullet.kt
+++ MyGame/app/src/main/java/com/example/mygame/domain/bullet/Bullet.kt
@@ -2,8 +2,6 @@ package com.example.mygame.domain.bullet
 
 import android.graphics.Bitmap
 import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Color
 import android.graphics.Matrix
 import android.graphics.RectF
 import com.example.mygame.UI.IDrawable
@@ -21,13 +19,13 @@ class Bullet(
     override var isDisappeared = false
 
     override val left: Float
-        get() = x - WIDTH / 2
+        get() = x - RADIUS / 2
     override val right: Float
-        get() = x + WIDTH / 2
+        get() = x + RADIUS / 2
     override val top: Float
-        get() = y - HEIGHT / 2
+        get() = y - RADIUS / 2
     override val bottom: Float
-        get() = y + HEIGHT / 2
+        get() = y + RADIUS / 2
 
     private var speedY = 0f
 
@@ -52,10 +50,6 @@ class Bullet(
         visitor.visit(this)
     }
 
-    private val paint = Paint().apply {
-        color = Color.RED
-    }
-
     override fun draw(canvas: Canvas) {
         val matrix = Matrix()
 
@@ -67,15 +61,11 @@ class Bullet(
         matrix.postScale(scaleX, scaleY)
         matrix.postTranslate(left, top)
 
-        // Поворачиваем объект вокруг его центра
-        matrix.postRotate(angle, x, y)
-
         canvas.drawBitmap(image, matrix, null)
     }
 
     companion object {
-        private const val WIDTH = 40f
-        private const val HEIGHT = 100f
+        private const val RADIUS = 50f
         private const val DEFAULT_ACCELERATION = 2200f // Ускорение по умолчанию
     }
 }
