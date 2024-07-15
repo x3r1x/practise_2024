0976a0ec5b80b74024b7ccfbd1edeb7d8fe594a2
impronez <orzaev7312@gmail.com>
2024-07-15 10:09:00 +0300

    Поправил коллизию пули с врагами

--- MyGame/app/src/main/java/com/example/mygame/domain/visitor/BulletCollisionVisitor.kt
+++ MyGame/app/src/main/java/com/example/mygame/domain/visitor/BulletCollisionVisitor.kt
@@ -1,15 +1,15 @@
 package com.example.mygame.domain.visitor
 
 import android.graphics.RectF
-import com.example.mygame.domain.IVisitor
-import com.example.mygame.domain.bullet.Bullet
+import android.util.Log
 import com.example.mygame.domain.Enemy
-import com.example.mygame.domain.player.Player
+import com.example.mygame.domain.IVisitor
 import com.example.mygame.domain.Platform
-import com.example.mygame.domain.Score
 import com.example.mygame.domain.bonuses.Jetpack
 import com.example.mygame.domain.bonuses.Shield
 import com.example.mygame.domain.bonuses.Spring
+import com.example.mygame.domain.bullet.Bullet
+import com.example.mygame.domain.player.Player
 
 class BulletCollisionVisitor(private val bullet: Bullet) : IVisitor {
 
@@ -35,11 +35,12 @@ class BulletCollisionVisitor(private val bullet: Bullet) : IVisitor {
 
     override fun visit(enemy: Enemy) {
         // Если пуля сталкивается с врагом, выключаем врага
+        Log.i("enemy coll", "hit")
         if (!bullet.isDisappeared && !enemy.isDisappeared) {
             val bulletRect = RectF(bullet.left, bullet.top, bullet.right, bullet.bottom)
             val enemyRect = RectF(enemy.left, enemy.top, enemy.right, enemy.bottom)
 
-            if (RectF.intersects(bulletRect, enemyRect)) {
+            if (bulletRect.intersect(enemyRect)) {
                 bullet.isDisappeared = true
                 enemy.isDisappeared = true
             }
