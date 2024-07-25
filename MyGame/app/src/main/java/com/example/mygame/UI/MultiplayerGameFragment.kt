7699ba90cd1c61961c598e5d01c90f46aecef8f1
impronez <orzaev7312@gmail.com>
2024-07-25 17:11:00 +0300

    Вынес функции

--- MyGame/app/src/main/java/com/example/mygame/UI/MultiplayerGameFragment.kt
+++ MyGame/app/src/main/java/com/example/mygame/UI/MultiplayerGameFragment.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
-import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.view.WindowMetrics
@@ -19,7 +18,6 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import com.example.mygame.R
-import com.example.mygame.domain.gameplay.MultiplayerGameplay
 import com.example.mygame.domain.gameplay.Type
 import com.example.mygame.presentation.GameViewModel
 
@@ -28,11 +26,29 @@ class MultiplayerGameFragment : Fragment() {
     private lateinit var scoreView: TextView
     private val gameViewModel: GameViewModel by viewModels()
 
-    private lateinit var readyButton: Button
+    private lateinit var helloGroup : ConstraintLayout
+    private lateinit var resultGroup : ConstraintLayout
+    private lateinit var waitingGroup : ConstraintLayout
+
+    private lateinit var helloTextView: TextView
+    private lateinit var resultTextView: TextView
+
+    private lateinit var goToMenuButton : Button
+    private lateinit var waitingReturnButton : Button
 
     private fun initViews(view: View) {
         gameView = view.findViewById(R.id.gameView)
         scoreView = view.findViewById(R.id.scoreView)
+
+        helloGroup = view.findViewById(R.id.helloGroup)
+        resultGroup = view.findViewById(R.id.resultGroup)
+        waitingGroup = view.findViewById(R.id.waitingGroup)
+
+        helloTextView = view.findViewById(R.id.helloTextView)
+        resultTextView = resultGroup.findViewById(R.id.winnerText)
+
+        goToMenuButton = resultGroup.findViewById(R.id.returnToMenuButton)
+        waitingReturnButton = view.findViewById(R.id.waitReturnButton)
     }
 
     @RequiresApi(Build.VERSION_CODES.R)
@@ -40,6 +56,42 @@ class MultiplayerGameFragment : Fragment() {
         val windowMetrics: WindowMetrics = requireActivity().windowManager.currentWindowMetrics
         val bounds = windowMetrics.bounds
         return Pair(bounds.width().toFloat(), bounds.height().toFloat())
+    }
+
+    private fun viewWaitingGroup(view: View) {
+        waitingGroup.visibility = VISIBLE
+        waitingReturnButton.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_multiplayerGameFragment_to_startFragment)
+        }
+    }
+
+    private fun viewLobby(view: View) {
+        waitingGroup.visibility = GONE
+        helloTextView.text = "Hello, Player ${gameViewModel.gameplay.id}!"
+        helloGroup.visibility = VISIBLE
+    }
+
+    private fun viewGame(view: View) {
+        helloGroup.visibility = GONE
+        gameView.visibility = VISIBLE
+    }
+
+    private fun viewEnd(view: View) {
+        val id = gameViewModel.gameplay.id
+        val winnerId = gameViewModel.gameplay.winnerId
+        gameView.alpha = 0.2f
+
+        if (id == winnerId) {
+            resultTextView.text = "You won!"
+        } else {
+            resultTextView.text = "You lose! Player ${winnerId} won!"
+        }
+
+        resultGroup.visibility = VISIBLE
+
+        goToMenuButton.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_multiplayerGameFragment_to_startFragment)
+        }
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -54,51 +106,19 @@ class MultiplayerGameFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_multiplayer_game, container, false)
         initViews(view)
 
-        val helloGroup = view.findViewById<ConstraintLayout>(R.id.helloGroup)
-        val helloTextView = view.findViewById<TextView>(R.id.helloTextView)
-
-        val resultGroup = view.findViewById<ConstraintLayout>(R.id.resultGroup)
-        val resultTextView = resultGroup.findViewById<TextView>(R.id.winnerText)
-        val goToMenuButton = resultGroup.findViewById<Button>(R.id.returnToMenuButton)
-
-        val waitingGroup = view.findViewById<ConstraintLayout>(R.id.waitingGroup)
-        val waitingReturnButton = view.findViewById<Button>(R.id.waitReturnButton)
-
         gameViewModel.initialize(screenWidth, screenHeight, GameViewModel.Type.MULTIPLAYER, requireContext(), lifecycleScope)
 
         gameViewModel.gameplay.gameState.observe(viewLifecycleOwner) {
             if (gameViewModel.gameplay.id == 0) {
-                waitingGroup.visibility = VISIBLE
-                waitingReturnButton.setOnClickListener {
-                    Navigation.findNavController(view).navigate(R.id.action_multiplayerGameFragment_to_startFragment)
-                }
+                viewWaitingGroup(view)
             } else if (gameViewModel.gameplay.gameState.value!!.type == Type.LOBBY) {
-                waitingGroup.visibility = GONE
-                helloTextView.text = "Hello, Player ${gameViewModel.gameplay.id}!"
-                helloGroup.visibility = VISIBLE
+                viewLobby(view)
             } else if (gameViewModel.gameplay.gameState.value!!.type == Type.GAME) {
-                helloGroup.visibility = GONE
-                gameView.visibility = VISIBLE
+                viewGame(view)
             } else if (gameViewModel.gameplay.gameState.value!!.type == Type.END) {
-                val id = gameViewModel.gameplay.id
-                val winnerId = gameViewModel.gameplay.winnerId
-                gameView.alpha = 0.2f
-
-                if (id == winnerId) {
-                    resultTextView.text = "You won!"
-                } else {
-                    resultTextView.text = "You lose! Player ${winnerId} won!"
-                }
-
-                resultGroup.visibility = VISIBLE
-
-                goToMenuButton.setOnClickListener {
-                    Navigation.findNavController(view).navigate(R.id.action_multiplayerGameFragment_to_startFragment)
-                }
+                viewEnd(view)
             }
         }
-
-
 
         return view
     }
